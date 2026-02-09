dc6b920383f27a5ef359168627ccd75bf4edfe6b
abhishekgautam6060 <abhishekgautam6060@gmail.com>
2026-02-09 23:47:58 +0530

    Testing

--- js/signup.js
+++ js/signup.js
@@ -1,22 +1,30 @@
 const HOST_URL ="https://seatmanager-backend.onrender.com";
 
+console.log(" Signup JS Loaded", {HOST_URL});
+
 function signup() {
 
     const name = document.getElementById("name").value;
     const phone = document.getElementById("phone").value;
     const password = document.getElementById("password").value;
 
-    fetch(`${HOST_URL}/api/auth/signup`, {
+    console.log("name", name);
+    console.log("phone", phone);
+    console.log("password", password);
+
+
+    console.log(" full URL ", `${HOST_URL}/api/auth/signup`);
+    fetch(`${HOST_URL}/api/auth/signup`, {    
         method: "POST",
         credentials: "include", // 🔥 IMPORTANT
         headers: {
             "Content-Type": "application/json"
-        },
+        },        
         body: JSON.stringify({
             name: name,
             phone: phone,
             password: password
-        })
+        })        
     })
     .then(res => {
         if (res.ok) {
